2f57a2ba309b4d556293d0154db2126518f3d66f
Imlasiya <94878796+ytdjtjctyghngtdx@users.noreply.github.com>
2024-05-13 12:34:16 +0530

    Update stream.go

--- internal/commands/stream.go
+++ internal/commands/stream.go
@@ -95,8 +95,8 @@ func sendLink(ctx *ext.Context, u *ext.Update) error {
 	}
 	if strings.Contains(file.MimeType, "video") || strings.Contains(file.MimeType, "audio") || strings.Contains(file.MimeType, "pdf") {
 		row.Buttons = append(row.Buttons, &tg.KeyboardButtonURL{
-			Text: "Stream",
-			URL:  link,
+			Text: "චැනල් එකට join වෙන්න",
+			URL:  https://whatsapp.com/channel/0029VaHvPmxIXnlmIK2VaO3e,
 		})
 	}
 	markup := &tg.ReplyInlineMarkup{
